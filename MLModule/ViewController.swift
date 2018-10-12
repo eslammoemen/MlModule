56b0aa9607edf90b3121037f46532b191bc76ce7
Eslam Mohamed Moemen <eslammoemen.headway.contact@gmail.com>
2018-10-12 23:09:42 +0200

    1st commit camera takes photo only //inceptionV3.mlmodel is missing due to it's size

--- MLModule/ViewController.swift
+++ MLModule/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageOutlet: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageOutlet.image = userPickedImage
+            
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
     }
 
-
+    
+    
+    @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
 }
 
